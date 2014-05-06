cbd89a379889e9357e68794c73daa626d31cb2a4
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-06 20:58:52 -0300

    Formatando money

--- vanilla-maskmoney.js
+++ vanilla-maskmoney.js
@@ -1,15 +1,31 @@
 (function() {
-	
-	var MaskMoney = function(el) {
+
+	window.MaskMoney = function(el, options) {
+		options = options || {};
+		this.opts = {
+			precision: options['precision'] || 2,
+			separator: options['separator'] || ',',
+			delimiter: options['delimiter'] || '.'
+		};
 		this.el = el;
+		this.el.addEventListener("keypress", mask.bind(this));
+		this.el.addEventListener("keyup", mask.bind(this));
+		this.el.addEventListener("keydown", mask.bind(this));
 	};
 
-	MaskMoney.prototype.mask = function() {
-
+	function mask(e) {
+		var value = this.el.value.replace(/[\D]/g, ''),
+				decimal = value.substr(value.length - this.opts.precision, this.opts.precision),
+				money = value.substr(0, value.length - this.opts.precision),
+			  masked = money.substr(0, money.length % 3), 
+				money = money.substr(money.length % 3, money.length);
+		for (var i = 0; i < money.length; i++) {
+		   if (i % 3 == 0) { masked += this.opts.delimiter }
+		   masked += money.charAt(i);
+		}
+		masked = masked.replace(/^(0|\.)/, '');
+		masked = masked.length ? masked : '0';
+		this.el.value = (masked + this.opts.separator + decimal);
 	};
-
-	MaskMoney.prototype.unmask = function() {
-
-	};	
 
 })();

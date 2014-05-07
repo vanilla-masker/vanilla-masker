e14bfdc1adf01bfacc8549bb379ab62f0d161708
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-07 20:29:51 -0300

    Improving the sintax

--- vanilla-masker.js
+++ vanilla-masker.js
@@ -10,41 +10,40 @@
 		};
 	};
 
-	function onType(e) {
-		e.target.value = this.mask(e.target.value);
+	VanillaMasker.prototype.maskMoneyElement = function(el) {
+		function onType(e) {
+			e.target.value = this.maskMoney(e.target.value);
+		};
+		if (el instanceof NodeList || el instanceof Array) {
+			for (var i = 0, len = el.length; i < len; i++) {
+				el.item(i).addEventListener("keypress", onType.bind(this));
+				el.item(i).addEventListener("keyup", onType.bind(this));
+				el.item(i).addEventListener("keydown", onType.bind(this));	
+			}
+		} else {
+			el.addEventListener("keypress", onType.bind(this));
+			el.addEventListener("keyup", onType.bind(this));
+			el.addEventListener("keydown", onType.bind(this));
+		}
 	};
 
-	VanillaMasker.prototype.maskElement = function(el) {
-		el.addEventListener("keypress", onType.bind(this));
-		el.addEventListener("keyup", onType.bind(this));
-		el.addEventListener("keydown", onType.bind(this));
-	};
-
-	VanillaMasker.prototype.unmask = function(money) {
-		return money.toString().replace(/[\D]/g, '');
-	};
-
-	VanillaMasker.prototype.mask = function(number) {
+	VanillaMasker.prototype.maskMoney = function(number) {
 		var 
-			precision = this.opts.precision,
-			delimiter = this.opts.delimiter,
-			separator = this.opts.separator,
-			unit = this.opts.unit,
 			value = number.toString().replace(/[\D]/g, ''),
-			unitSpace = unit.length ? ' ' : '',
-			clearDelimiter = new RegExp("^(0|\\"+ delimiter +")"),
-			decimal = value.substr(value.length - precision, precision),
-			money = value.substr(0, value.length - precision),
+			unitSpace = this.opts.unit.length ? ' ' : '',
+			clearDelimiter = new RegExp("^(0|\\"+ this.opts.delimiter +")"),
+			decimal = value.substr(value.length - this.opts.precision, this.opts.precision),
+			money = value.substr(0, value.length - this.opts.precision),
 		  masked = money.substr(0, money.length % 3), 
 			money = money.substr(money.length % 3, money.length)
 		;
-		for (var i = 0; i < money.length; i++) {
-		   (i % 3 == 0) && (masked += delimiter);
-		   masked += money.charAt(i);
+		for (var i = 0, len = money.length; i < len; i++) {
+			(i % 3 == 0) && (masked += this.opts.delimiter);
+			masked += money.charAt(i);
 		}
 		masked = masked.replace(clearDelimiter, '');
 		masked = masked.length ? masked : '0';
-		return (unit + unitSpace + masked + separator + decimal);
+		return (this.opts.unit + unitSpace + masked + this.opts.separator + decimal);
 	};
 
 	window.VanillaMasker = VanillaMasker;

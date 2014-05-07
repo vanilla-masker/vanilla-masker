1aaa759ee2d7139bafc78db84d781ff026751356
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-07 18:14:21 -0300

    Incluindo maskElement, mask e unmask

--- vanilla-maskmoney.js
+++ vanilla-maskmoney.js
@@ -1,31 +1,52 @@
 (function() {
 
-	window.MaskMoney = function(el, options) {
+	var MaskMoney = function(options) {
 		options = options || {};
 		this.opts = {
 			precision: options['precision'] || 2,
 			separator: options['separator'] || ',',
-			delimiter: options['delimiter'] || '.'
+			delimiter: options['delimiter'] || '.',
+			unit: options['unit'] || ''
 		};
-		this.el = el;
-		this.el.addEventListener("keypress", mask.bind(this));
-		this.el.addEventListener("keyup", mask.bind(this));
-		this.el.addEventListener("keydown", mask.bind(this));
 	};
 
-	function mask(e) {
-		var value = this.el.value.replace(/[\D]/g, ''),
-				decimal = value.substr(value.length - this.opts.precision, this.opts.precision),
-				money = value.substr(0, value.length - this.opts.precision),
-			  masked = money.substr(0, money.length % 3), 
-				money = money.substr(money.length % 3, money.length);
+	function onType(e) {
+		e.target.value = this.mask(e.target.value);
+	};
+
+	MaskMoney.prototype.maskElement = function(el) {
+		el.addEventListener("keypress", onType.bind(this));
+		el.addEventListener("keyup", onType.bind(this));
+		el.addEventListener("keydown", onType.bind(this));
+	};
+
+	MaskMoney.prototype.unmask = function(money) {
+		return money.toString().replace(/[\D]/g, '');
+	};
+
+	MaskMoney.prototype.mask = function(number) {
+		var 
+			precision = this.opts.precision,
+			delimiter = this.opts.delimiter,
+			separator = this.opts.separator,
+			unit = this.opts.unit,
+			value = number.toString().replace(/[\D]/g, ''),
+			unitSpace = unit.length ? ' ' : '',
+			clearDelimiter = new RegExp("^(0|\\"+ delimiter +")"),
+			decimal = value.substr(value.length - precision, precision),
+			money = value.substr(0, value.length - precision),
+		  masked = money.substr(0, money.length % 3), 
+			money = money.substr(money.length % 3, money.length)
+		;
 		for (var i = 0; i < money.length; i++) {
-		   if (i % 3 == 0) { masked += this.opts.delimiter }
+		   (i % 3 == 0) && (masked += delimiter);
 		   masked += money.charAt(i);
 		}
-		masked = masked.replace(/^(0|\.)/, '');
+		masked = masked.replace(clearDelimiter, '');
 		masked = masked.length ? masked : '0';
-		this.el.value = (masked + this.opts.separator + decimal);
+		return (unit + unitSpace + masked + separator + decimal);
 	};
+
+	window.MaskMoney = MaskMoney;
 
 })();

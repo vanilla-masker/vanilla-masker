e353f63d9e17d067d852ab210b20284a3314a53a
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-08 11:37:10 -0300

    Clean opts in the constructor

--- src/vanilla-masker.js
+++ src/vanilla-masker.js
@@ -4,8 +4,8 @@
 		opts = opts || {};
 		this.opts = {
 			precision: opts.hasOwnProperty('precision') ? opts.precision : 2,
-			separator: opts.hasOwnProperty('separator') ? opts.separator : ',',
-			delimiter: opts.hasOwnProperty('delimiter') ? opts.delimiter : '.',
+			separator: opts['separator'] || ',',
+			delimiter: opts['delimiter'] || '.',
 			unit: opts['unit'] || ''
 		};
 	};

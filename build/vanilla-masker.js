b222dcf21fa8957edb0a3f8c04e4221fe4f2b38e
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-11 11:39:19 -0300

    Update build

--- build/vanilla-masker.js
+++ build/vanilla-masker.js
@@ -4,13 +4,13 @@
 		opts = opts || {};
 		this.opts = {
 			precision: opts.hasOwnProperty('precision') ? opts.precision : 2,
-			separator: opts['separator'] || ',',
-			delimiter: opts['delimiter'] || '.',
-			unit: opts['unit'] && (opts['unit'] + ' ') || '',
-			zeroCents: opts['zeroCents']
+			separator: opts.separator || ',',
+			delimiter: opts.delimiter || '.',
+			unit: opts.unit && (opts.unit + ' ') || '',
+			zeroCents: opts.zeroCents
 		};
 		this.lastOutput = '';
-		this.moneyPrecision = opts['zeroCents'] ? 0 : this.opts.precision;
+		this.moneyPrecision = opts.zeroCents ? 0 : this.opts.precision;
 	};
 
 	window.VanillaMasker = VanillaMasker;
@@ -20,8 +20,8 @@
 
 	VanillaMasker.prototype.maskMoney = function(el) {
 		var that = this
-			, elements = [].slice.call(el)
-			, elements = elements.length ? elements : [el]
+			, elSliced = [].slice.call(el)
+			, elements = elSliced.length ? elSliced : [el]
 			, onType = function(e) {
 				e.target.value = that.toMoney(e.target.value);
 			}
@@ -40,27 +40,31 @@
 	VanillaMasker.prototype.toMoney = function(input) {
 		var precision = this.opts.precision
 			, separator = this.opts.separator
+			, delimiter = this.opts.delimiter
+			, unit = this.opts.unit
 		;
 		if (this.opts.zeroCents) {
 			var zeroMatcher = ("("+ separator +"[0]{0,"+ precision +"})")
 				, zeroRegExp = new RegExp(zeroMatcher, "g")
 				, initialInputLength = input.length || 0
-				, input = input.toString().replace(zeroRegExp, '')
 			;
+			input = input.toString().replace(zeroRegExp, '');
 			if (initialInputLength < this.lastOutput.length) {
 				input = input.slice(0, input.length - 1);
 			}
 		}
 		var number = input.toString().replace(/[\D]/g, '')
-			, clearDelimiter = new RegExp("^(0|\\"+ this.opts.delimiter +")")
+			, clearDelimiter = new RegExp("^(0|\\"+ delimiter +")")
 			, clearSeparator = new RegExp("(\\"+ separator +")$")
 			, money = number.substr(0, number.length - this.moneyPrecision)
 			, masked = money.substr(0, money.length % 3)
-			, money = money.substr(money.length % 3, money.length)
 			, cents = new Array(precision + 1).join('0')
 		;
+		money = money.substr(money.length % 3, money.length);
 		for (var i = 0, len = money.length; i < len; i++) {
-			(i % 3 == 0) && (masked += this.opts.delimiter);
+			if (i % 3 === 0) {
+				masked += delimiter;
+			}
 			masked += money.charAt(i);
 		}
 		masked = masked.replace(clearDelimiter, '');
@@ -72,7 +76,7 @@
 			var centsSliced = (precision > centsLength) ? precision : centsLength;
 			cents = (cents + centsValue).slice(-centsSliced);
 		}
-		var output = (this.opts.unit + masked + separator + cents);
+		var output = (unit + masked + separator + cents);
 		this.lastOutput = output = output.replace(clearSeparator, '');
 		return output;
 	};
@@ -81,8 +85,8 @@
 
 	VanillaMasker.prototype.maskNumber = function(el) {
 		var that = this
-			, elements = [].slice.call(el)
-			, elements = elements.length ? elements : [el]
+			, elSliced = [].slice.call(el)
+			, elements = elSliced.length ? elSliced : [el]
 			, onType = function(e) {
 				e.target.value = that.toNumber(e.target.value);
 			}

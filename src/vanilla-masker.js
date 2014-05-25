14cfcefb9545778d55f1bc85c0aca05ad2df3ca2
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2014-05-25 14:30:21 -0300

    maskPhone functions (not finished)

--- src/vanilla-masker.js
+++ src/vanilla-masker.js
@@ -7,10 +7,14 @@
       separator: opts.separator || ',',
       delimiter: opts.delimiter || '.',
       unit: opts.unit && (opts.unit + ' ') || '',
-      zeroCents: opts.zeroCents
+      zeroCents: opts.zeroCents,
+      phone: opts.phone || '(99) 9999-9999'
     };
     this.lastOutput = '';
     this.moneyPrecision = opts.zeroCents ? 0 : this.opts.precision;
+    this.DIGIT = '9';
+    this.ALPHA = 'A';
+    this.WILDCARD = '?';
   };
 
   window.VanillaMasker = VanillaMasker;
